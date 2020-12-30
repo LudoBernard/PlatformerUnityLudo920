7e2f74f83b8176ee2fa286629d8719f44294c02e
LudoBernard <ludo.bernard.pro@gmail.com>
2020-12-30 18:08:14 +0100

    Changed blocks and optimized some code

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,11 +8,14 @@ using UnityEngine.AI;
 public class Block : MonoBehaviour
 {
     [SerializeField] private GameObject keyGameObject_;
+    [SerializeField] private GameObject gemGameObject_;
+    [SerializeField] private bool hasKey_ = false;
     
     [FMODUnity.EventRef][SerializeField] private string breakEvent_ = "";
 
     private GameObject key_;
-    private Vector2 keyForce_ = new Vector2(0f, 200f);
+    private GameObject gem_;
+    private Vector2 objectForce_ = new Vector2(0f, 200f);
     private int lives_ = 2;
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -21,9 +24,20 @@ public class Block : MonoBehaviour
         {
             if (gameObject.CompareTag("GoldenBlock"))
             {
-                key_ = Instantiate(keyGameObject_, gameObject.transform.position, Quaternion.identity);
-                key_.GetComponent<Rigidbody2D>().AddForce(keyForce_);
-                Destroy(gameObject);
+                if (hasKey_)
+                {
+                    key_ = Instantiate(keyGameObject_, gameObject.transform.position, Quaternion.identity);
+                    key_.GetComponent<Rigidbody2D>().AddForce(objectForce_);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    gem_ = Instantiate(gemGameObject_, gameObject.transform.position, Quaternion.identity);
+                    gem_.GetComponent<GemsFloating>().enabled = false;
+                    gem_.AddComponent<Rigidbody2D>();
+                    gem_.GetComponent<Rigidbody2D>().AddForce(objectForce_);
+                    Destroy(gameObject);
+                }
             }
 
             lives_--;
