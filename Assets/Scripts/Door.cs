5dd06e7f8381e3af7eadbf26ef19a8883d95898c
LudoBernard <ludo.bernard.pro@gmail.com>
2020-12-30 18:12:52 +0100

    Added door file

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Animator animTop_;
+    [SerializeField] private Animator animBot_;
+
+    [SerializeField] private PlayerCharacter player_;
+
+    private void Start()
+    {
+        animBot_.Play("BottomDoorClosed");
+        animTop_.Play("TopDoorClosed");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && player_.hasKey_)
+        {
+            animBot_.Play("BottomDoorOpen");
+            animTop_.Play("TopDoorOpen");
+        }
+    }
+}
