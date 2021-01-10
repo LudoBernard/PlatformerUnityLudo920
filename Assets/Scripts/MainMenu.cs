d26ec1f7ebcd39f213251484be21a7b8c0538bca
LudoBernard <ludo.bernard.pro@gmail.com>
2021-01-10 19:21:49 +0100

    Fixed some bugs that prevented building the game, all done!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -16,7 +16,6 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     public void PlaySound()
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -50,7 +50,6 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     public void PlaySound()
